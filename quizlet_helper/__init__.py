b9918f791aa80e04c9263f35ddea888d36863efe
Yubo-Cao <cao2006721@gmail.com>
2022-09-04 19:39:03 -0400

    feat(error): export log to user.

--- quizlet_helper/__init__.py
+++ quizlet_helper/__init__.py
@@ -1,6 +1,7 @@
 from quizlet_helper.study_set import StudySet, Card
 from quizlet_helper.folder import Folder
 from quizlet_helper.user import User
+from quizlet_helper.error import log
 from playwright.sync_api import sync_playwright
 
 __all__ = [
@@ -9,4 +10,5 @@ __all__ = [
     'Folder',
     'User',
     'sync_playwright',
+    'log',
 ]
